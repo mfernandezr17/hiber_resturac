b954dbf6b9b18ba5d39bdf636c204cd4fa08d587
mfernandezr17 <105849432+mfernandezr17@users.noreply.github.com>
2022-05-19 11:22:13 +0200

    Add files via upload

---
+++ php-poo/emtodosyatributos.php
@@ -0,0 +1,10 @@
+<?php
+    class miClase
+    //declarar atributos
+    public $atributo1="";
+    //declarar metodos
+    public function miMetodo(){
+        //
+    }
+
+?>
---
+++ php-poo/index.php
@@ -0,0 +1,12 @@
+<?php
+    //Una clase es nuestro molde de trabajo para neustros metodos y atributos
+
+    class miClase{
+        //Definir los atributos
+
+        //Definir los metodos
+    }
+    class MiClase2{
+
+    }
+?>
